78f5148ac506bf144f9fbcd0bb82fb7b034496be
Pamela <fracassa.1080099@studenti.uniroma1.it>
2019-03-05 15:45:48 +0100

    removed google maps + minor improvments

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -4,50 +4,53 @@
  */
 var dbPromise;
 class DBHelper {
-
   /**
    * Database URL.
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 1337 // Change this to your server port
+    const port = 1337; // Change this to your server port
     return `http://localhost:${port}/restaurants/`;
   }
   static openDatabase() {
-  return idb.open('Restaurant Reviews', 5, (upgradeDBObject) => {
-    switch (upgradeDBObject.oldVersion) {
-      case 0:
-        upgradeDBObject.createObjectStore('restaurants', {
-          keyPath: 'id'
-        });
-      case 1:
-        upgradeDBObject.createObjectStore('restaurant-reviews', {
-          keyPath: 'id'
-        });
+    return idb.open("Restaurant Reviews", 5, upgradeDBObject => {
+      switch (upgradeDBObject.oldVersion) {
+        case 0:
+          upgradeDBObject.createObjectStore("restaurants", {
+            keyPath: "id"
+          });
+        case 1:
+          upgradeDBObject.createObjectStore("restaurant-reviews", {
+            keyPath: "id"
+          });
         case 2:
-        upgradeDBObject.createObjectStore('outbox', {
-          autoIncrement: true, 
-          keyPath: 'id' 
-        });
-    }
-  })
-}
-//OLD Version 
+          upgradeDBObject.createObjectStore("outbox", {
+            autoIncrement: true,
+            keyPath: "id"
+          });
+        case 2:
+          upgradeDBObject.createObjectStore("favorite", {
+            autoIncrement: true,
+            keyPath: "id"
+          });
+      }
+    });
+  }
+  //OLD Version
   // static openDatabase() {
   //   return idb.open('restaurants' , 1  , function(upgradeDb) {
   //     upgradeDb.createObjectStore('restaurants' ,{keyPath: 'id'});
   //   });
   // }
-  static getCachedMessages(){
+  static getCachedMessages() {
     dbPromise = DBHelper.openDatabase();
-    return dbPromise.then(function(db){
-
+    return dbPromise.then(function(db) {
       //if we showing posts or very first time of the page loading.
       //we don't need to go to idb
-      if(!db) return;
+      if (!db) return;
 
-      var tx = db.transaction('restaurants');
-      var store = tx.objectStore('restaurants');
+      var tx = db.transaction("restaurants");
+      var store = tx.objectStore("restaurants");
 
       return store.getAll();
     });
@@ -56,42 +59,44 @@ class DBHelper {
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    DBHelper.getCachedMessages().then(function(data){
+    DBHelper.getCachedMessages().then(function(data) {
       // if we have data to show then we pass it immediately.
-      if(data.length > 0){
-        return callback(null , data);
+      if (data.length > 0) {
+        return callback(null, data);
       }
 
       // After passing the cached messages.
       // We need to update the cache with fetching restaurants from network.
-      fetch(DBHelper.DATABASE_URL , {credentials:'same-origin'})
-      .then(res => {
-       
-        return res.json()})
-      .then(data => {
-        dbPromise.then(function(db){
-          if(!db) return db;
-         
-          var tx = db.transaction('restaurants' , 'readwrite');
-          var store = tx.objectStore('restaurants');
+      fetch(DBHelper.DATABASE_URL, { credentials: "same-origin" })
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          dbPromise.then(function(db) {
+            if (!db) return db;
 
-          data.forEach(restaurant => store.put(restaurant));
+            var tx = db.transaction("restaurants", "readwrite");
+            var store = tx.objectStore("restaurants");
 
-          // limit the data for 30
-          store.openCursor(null , 'prev').then(function(cursor){
-            return cursor.advance(30);
-          })
-          .then(function deleteRest(cursor){
-            if(!cursor) return;
-            cursor.delete();
-            return cursor.continue().then(deleteRest);
+            data.forEach(restaurant => store.put(restaurant));
+
+            // limit the data for 30
+            store
+              .openCursor(null, "prev")
+              .then(function(cursor) {
+                return cursor.advance(30);
+              })
+              .then(function deleteRest(cursor) {
+                if (!cursor) return;
+                cursor.delete();
+                return cursor.continue().then(deleteRest);
+              });
           });
+          return callback(null, data);
+        })
+        .catch(err => {
+          return callback(err, null);
         });
-        return callback(null,data);
-      })
-      .catch(err => {
-        return callback(err , null)
-      });
     });
   }
 
@@ -100,12 +105,19 @@ class DBHelper {
    */
   static fetchRestaurantById(id, callback) {
     // ** Using fetch API *** //
-    fetch(`${DBHelper.DATABASE_URL}${id}`).then(response => {
-      if (response){
-        return response.json();
-      }
-    }).then(json => callback(null, json))
-       .catch(err => callback(`Request failed. Returned status of ${err.code}. ${err.message}`,null) );
+    fetch(`${DBHelper.DATABASE_URL}${id}`)
+      .then(response => {
+        if (response) {
+          return response.json();
+        }
+      })
+      .then(json => callback(null, json))
+      .catch(err =>
+        callback(
+          `Request failed. Returned status of ${err.code}. ${err.message}`,
+          null
+        )
+      );
   }
 
   /**
@@ -143,17 +155,23 @@ class DBHelper {
   /**
    * Fetch restaurants by a cuisine and a neighborhood with proper error handling.
    */
-  static fetchRestaurantByCuisineAndNeighborhood(cuisine, neighborhood, callback) {
+  static fetchRestaurantByCuisineAndNeighborhood(
+    cuisine,
+    neighborhood,
+    callback
+  ) {
     // Fetch all restaurants
     DBHelper.fetchRestaurants((error, restaurants) => {
       if (error) {
         callback(error, null);
       } else {
-        let results = restaurants
-        if (cuisine != 'all') { // filter by cuisine
+        let results = restaurants;
+        if (cuisine != "all") {
+          // filter by cuisine
           results = results.filter(r => r.cuisine_type == cuisine);
         }
-        if (neighborhood != 'all') { // filter by neighborhood
+        if (neighborhood != "all") {
+          // filter by neighborhood
           results = results.filter(r => r.neighborhood == neighborhood);
         }
         callback(null, results);
@@ -171,9 +189,13 @@ class DBHelper {
         callback(error, null);
       } else {
         // Get all neighborhoods from all restaurants
-        const neighborhoods = restaurants.map((v, i) => restaurants[i].neighborhood)
+        const neighborhoods = restaurants.map(
+          (v, i) => restaurants[i].neighborhood
+        );
         // Remove duplicates from neighborhoods
-        const uniqueNeighborhoods = neighborhoods.filter((v, i) => neighborhoods.indexOf(v) == i)
+        const uniqueNeighborhoods = neighborhoods.filter(
+          (v, i) => neighborhoods.indexOf(v) == i
+        );
         callback(null, uniqueNeighborhoods);
       }
     });
@@ -189,9 +211,11 @@ class DBHelper {
         callback(error, null);
       } else {
         // Get all cuisines from all restaurants
-        const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type)
+        const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type);
         // Remove duplicates from cuisines
-        const uniqueCuisines = cuisines.filter((v, i) => cuisines.indexOf(v) == i)
+        const uniqueCuisines = cuisines.filter(
+          (v, i) => cuisines.indexOf(v) == i
+        );
         callback(null, uniqueCuisines);
       }
     });
@@ -201,47 +225,61 @@ class DBHelper {
    * Restaurant page URL.
    */
   static urlForRestaurant(restaurant) {
-    return (`./restaurant.html?id=${restaurant.id}`);
+    return `./restaurant.html?id=${restaurant.id}`;
   }
 
   /**
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}.jpg`);
-
+    return `/img/${restaurant.photograph}.jpg`;
   }
 
-  static imageSetUrlForRestaurant(restaurant){
-  return (`/images/${restaurant.id}-small.jpg 480w , /images/${restaurant.id}-large.jpg 1024w`);
+  static imageSetUrlForRestaurant(restaurant) {
+    return `/images/${restaurant.id}-small.jpg 480w , /images/${
+      restaurant.id
+    }-large.jpg 1024w`;
   }
 
   /**
    * Map marker for a restaurant.
    */
-  static mapMarkerForRestaurant(restaurant, map) {
-    const marker = new google.maps.Marker({
-      position: restaurant.latlng,
-      title: restaurant.name,
-      url: DBHelper.urlForRestaurant(restaurant),
-      map: map,
-      animation: google.maps.Animation.DROP}
+  static mapMarkerForRestaurant(restaurant, newMap) {
+    // https://leafletjs.com/reference-1.3.0.html#marker
+    const marker = new L.marker(
+      [restaurant.latlng.lat, restaurant.latlng.lng],
+      {
+        title: restaurant.name,
+        alt: `Localisation of ${restaurant.name} restaurant`,
+        url: DBHelper.urlForRestaurant(restaurant),
+        id: `marker-${restaurant.id}`
+      }
     );
+    marker.addTo(newMap);
     return marker;
   }
+
   static fetchReviewsForRestaurant(id, callback) {
-    fetch('http://localhost:1337/reviews/?restaurant_id=' + id).then(response => {
-      if (response.status === 200) {
-        response.json().then(json => {
-          callback(null, json);
-        }).catch(err => {
-          callback(err, null);
-        });
-      } else {
-        callback(`Request failed. Returned status of ${response.status}`, null);
-      }
-    }).catch(err => {
-      callback(err, null);
-    });
+    fetch("http://localhost:1337/reviews/?restaurant_id=" + id)
+      .then(response => {
+        if (response.status === 200) {
+          response
+            .json()
+            .then(json => {
+              callback(null, json);
+            })
+            .catch(err => {
+              callback(err, null);
+            });
+        } else {
+          callback(
+            `Request failed. Returned status of ${response.status}`,
+            null
+          );
+        }
+      })
+      .catch(err => {
+        callback(err, null);
+      });
   }
 }
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -4,74 +4,87 @@ var map;
 /**
  * Service Worker init
  * */
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-           .register('./service-worker.js')
-           .then(function() { console.log('Service Worker Registered'); });
-}
-
-/**
- * Initialize Google map, called from HTML.
- */
-window.initMap = () => {
-  fetchRestaurantFromURL((error, restaurant) => {
-    if (error) { // Got an error!
-      console.error(error);
-    } else {
-      self.map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 16,
-        center: restaurant.latlng,
-        scrollwheel: false
-      });
-      fillBreadcrumb();
-      DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
-    }
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./service-worker.js").then(function() {
+    console.log("Service Worker Registered");
   });
 }
+/**
+ * Initialize map as soon as the page is loaded.
+ */
+document.addEventListener("DOMContentLoaded", () => {
+  const restaurant = this.fetchRestaurantFromURL();
+});
+initMap = function(restaurant) {
+  if (typeof L !== "undefined") {
+    const container = L.DomUtil.get("map");
+    if (container !== null) container._leaflet_id = null;
+    if (!!self.currentMarker) self.currentMarker.remove();
+    console.log(restaurant.latlng.lat);
+    let lat = restaurant.latlng.lat;
+    let lgn = restaurant.latlng.lng;
+    const newMap = L.map("map", {
+      center: [lat, lgn],
+      zoom: 16,
+      scrollWheelZoom: false
+    });
+    L.tileLayer(
+      "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}",
+      {
+        mapboxToken:
+          "sk.eyJ1IjoicGFtbWl4IiwiYSI6ImNqc2xtNG05dTMweTkzem1senYyOXpwbHoifQ.DBUV6qnomOStDilr7eZklg",
+        maxZoom: 18,
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: "mapbox.streets"
+      }
+    ).addTo(newMap);
+    DBHelper.mapMarkerForRestaurant(restaurant, newMap);
+  }
+};
 /*
  * fetch reviews
  */
 fetchReviews = () => {
-  const id = getParameterByName('id');
+  const id = getParameterByName("id");
   if (!id) {
-    console.log('No ID in URL');
+    console.log("No ID in URL");
     return;
   }
   DBHelper.fetchReviewsForRestaurant(id, (err, reviews) => {
-  //  self.reviews = reviews;
-  self.restaurant.reviews = reviews;
-    if (err || !reviews) {
-      console.log('reviews fetch error', err);
-      return;
-    }
-    fillReviewsHTML();
+    fillReviewsHTML(reviews);
   });
-}
+};
 /*
  * set favorite button
  */
-setFavoriteButton = (status) => {
-  const favorite = document.getElementById('favBtn');
-  if (status === 'true') {
-    favorite.title = 'Restaurant is Favorite';
-    favorite.innerHTML = '⭐️ Mark as Unfavorite';
+setFavoriteButton = status => {
+  const favorite = document.getElementById("favBtn");
+  if (status === "true") {
+    favorite.title = "Restaurant is Favorite";
+    favorite.innerHTML = "⭐️ Mark as Unfavorite";
   } else {
-    favorite.title = 'Restaurant is not Favorite';
-    favorite.innerHTML = '☆ Mark as Favorite';
+    favorite.title = "Restaurant is not Favorite";
+    favorite.innerHTML = "☆ Mark as Favorite";
   }
-}
+};
 /**
  * Get current restaurant from page URL.
  */
-fetchRestaurantFromURL = (callback) => {
-  if (self.restaurant) { // restaurant already fetched!
-    callback(null, self.restaurant)
+fetchRestaurantFromURL = callback => {
+  console.log(self.restaurant);
+  if (self.restaurant) {
+    // restaurant already fetched!
+    // callback(null, self.restaurant);
     return;
   }
-  const id = getParameterByName('id');
-  if (!id) { // no id found in URL
-    error = 'No restaurant id in URL'
-    callback(error, null);
+  const id = getParameterByName("id");
+  if (!id) {
+    // no id found in URL
+    error = "No restaurant id in URL";
+    // callback(error, null);
   } else {
     DBHelper.fetchRestaurantById(id, (error, restaurant) => {
       self.restaurant = restaurant;
@@ -79,31 +92,32 @@ fetchRestaurantFromURL = (callback) => {
         console.error(error);
         return;
       }
+      initMap(restaurant);
       fillRestaurantHTML();
-      callback(null, restaurant)
+      // callback(null, restaurant);
     });
   }
-}
+};
 
 /**
  * Create restaurant HTML and add it to the webpage
  */
 fillRestaurantHTML = (restaurant = self.restaurant) => {
-  const name = document.getElementById('restaurant-name');
+  const name = document.getElementById("restaurant-name");
   name.innerHTML = restaurant.name;
-// favorite
-setFavoriteButton(restaurant.is_favorite);
+  // favorite
+  setFavoriteButton(restaurant.is_favorite);
 
-  const address = document.getElementById('restaurant-address');
+  const address = document.getElementById("restaurant-address");
   address.innerHTML = restaurant.address;
 
-  const image = document.getElementById('restaurant-img');
-  image.className = 'restaurant-img'
- image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.srcset = DBHelper.imageSetUrlForRestaurant(restaurant); 
- image.alt = restaurant.name + restaurant.photoDescription;
+  const image = document.getElementById("restaurant-img");
+  image.className = "restaurant-img";
+  image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.srcset = DBHelper.imageSetUrlForRestaurant(restaurant);
+  image.alt = restaurant.name + restaurant.photoDescription;
 
-  const cuisine = document.getElementById('restaurant-cuisine');
+  const cuisine = document.getElementById("restaurant-cuisine");
   cuisine.innerHTML = restaurant.cuisine_type;
 
   // fill operating hours
@@ -112,62 +126,66 @@ setFavoriteButton(restaurant.is_favorite);
   }
   // fill reviews
   //fillReviewsHTML();
-   fetchReviews();
-}
+  fetchReviews();
+};
 
 /**
  * Create restaurant operating hours HTML table and add it to the webpage.
  */
-fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => {
-  const hours = document.getElementById('restaurant-hours');
+fillRestaurantHoursHTML = (
+  operatingHours = self.restaurant.operating_hours
+) => {
+  const hours = document.getElementById("restaurant-hours");
   for (let key in operatingHours) {
-    const row = document.createElement('tr');
+    const row = document.createElement("tr");
 
-    const day = document.createElement('td');
+    const day = document.createElement("td");
     day.innerHTML = key;
     row.appendChild(day);
 
-    const time = document.createElement('td');
+    const time = document.createElement("td");
     time.innerHTML = operatingHours[key];
     row.appendChild(time);
 
     hours.appendChild(row);
   }
-}
+};
 
 /**
  * Create all reviews HTML and add them to the webpage.
  */
-fillReviewsHTML = (reviews = self.restaurant.reviews) => {
-  const container = document.getElementById('reviews-container');
-  const title = document.createElement('h3');
-  title.innerHTML = 'Reviews';
+fillReviewsHTML = reviews => {
+  const container = document.getElementById("reviews-container");
+  const title = document.createElement("h3");
+  title.innerHTML = "Reviews";
   container.appendChild(title);
 
   if (!reviews) {
-    const noReviews = document.createElement('p');
-    noReviews.innerHTML = 'No reviews yet!';
+    const noReviews = document.createElement("p");
+    noReviews.innerHTML = "No reviews yet!";
     container.appendChild(noReviews);
     return;
   }
-  const ul = document.getElementById('reviews-list');
+  const ul = document.getElementById("reviews-list");
   reviews.forEach(review => {
     ul.appendChild(createReviewHTML(review));
   });
   container.appendChild(ul);
-}
-formatDate = (ts) => {
+};
+formatDate = ts => {
   let date = new Date(ts);
-  return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
-}
+  return (
+    date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()
+  );
+};
 /**
  * Create review HTML and add it to the webpage.
  */
-createReviewHTML = (review) => {
-  const li = document.createElement('li');
-  const name = document.createElement('p');
+createReviewHTML = review => {
+  const li = document.createElement("li");
+  const name = document.createElement("p");
   name.innerHTML = review.name;
-  name.className = 'reviewerName';
+  name.className = "reviewerName";
   li.appendChild(name);
 
   // const date = document.createElement('p');
@@ -175,98 +193,98 @@ createReviewHTML = (review) => {
   // date.className = 'date';
   // li.appendChild(date);
 
-  const date = document.createElement('p');
+  const date = document.createElement("p");
   date.innerHTML = formatDate(review.createdAt);
-  date.className ='dateReview';
-  li.appendChild(date)
+  date.className = "dateReview";
+  li.appendChild(date);
 
-  const rating = document.createElement('p');
+  const rating = document.createElement("p");
   rating.innerHTML = `Rating: ${review.rating}`;
-  rating.className = 'rating';
+  rating.className = "rating";
   li.appendChild(rating);
 
-  const comments = document.createElement('p');
+  const comments = document.createElement("p");
   comments.innerHTML = review.comments;
-  comments.className = 'comments';
+  comments.className = "comments";
   li.appendChild(comments);
 
   return li;
-}
+};
 
 /*** Reviews ***/
-navigator.serviceWorker.ready.then(function (swRegistration) {
-  let form = document.querySelector('#review-form');
-  form.addEventListener('submit', e => {
+navigator.serviceWorker.ready.then(function(swRegistration) {
+  let form = document.querySelector("#review-form");
+  form.addEventListener("submit", e => {
     e.preventDefault();
-    let rating = form.querySelector('#rating');
+    let rating = form.querySelector("#rating");
     let review = {
-      restaurant_id: getParameterByName('id'),
-      name: form.querySelector('#name').value,
+      restaurant_id: getParameterByName("id"),
+      name: form.querySelector("#name").value,
       rating: rating.options[rating.selectedIndex].value,
-      comments: form.querySelector('#comment').value
+      comments: form.querySelector("#comment").value
     };
-    DBHelper.openDatabase().then(function(db){
-      var transaction = db.transaction('outbox', 'readwrite');
-      return transaction.objectStore('outbox').put(review);
-     
-    }).then(function () {
-      if (navigator.onLine){
-      const ul = document.getElementById('reviews-list');
-      review.createdAt = new Date(); 
-        ul.appendChild(createReviewHTML(review));
-      }
-      form.reset();
-      return swRegistration.sync.register('sync').then(() => {
-        console.log('Sync registered');
+    DBHelper.openDatabase()
+      .then(function(db) {
+        var transaction = db.transaction("outbox", "readwrite");
+        return transaction.objectStore("outbox").put(review);
+      })
+      .then(function() {
+        if (navigator.onLine) {
+          const ul = document.getElementById("reviews-list");
+          review.createdAt = new Date();
+          ul.appendChild(createReviewHTML(review));
+        }
+        form.reset();
+        return swRegistration.sync.register("sync").then(() => {
+          console.log("Sync registered");
+        });
       });
-    });
   });
 });
 
 /*** Favorites ***/
-navigator.serviceWorker.ready.then(function (swRegistration) {
-  let btn = document.getElementById('favBtn');
-  btn.addEventListener('click', e => {
-    const opposite = (self.restaurant.is_favorite === 'true') ? 'false' : 'true';
+navigator.serviceWorker.ready.then(function(swRegistration) {
+  let btn = document.getElementById("favBtn");
+  btn.addEventListener("click", e => {
+    const opposite = self.restaurant.is_favorite === "true" ? "false" : "true";
     let res = {
-      resId: getParameterByName('id'),
+      resId: getParameterByName("id"),
       favorite: opposite
     };
-    DBHelper.openDatabase().then(function(db){
-      var transaction = db.transaction('favorite', 'readwrite');
-      return transaction.objectStore('favorite').put(res);
-    }).then(function () {
-      setFavoriteButton(opposite);
-      self.restaurant.is_favorite = opposite;
-      return swRegistration.sync.register('favorite').then(() => {
-        console.log('Favorite Sync registered');
+    DBHelper.openDatabase()
+      .then(function(db) {
+        var transaction = db.transaction("favorite", "readwrite");
+        return transaction.objectStore("favorite").put(res);
+      })
+      .then(function() {
+        setFavoriteButton(opposite);
+        self.restaurant.is_favorite = opposite;
+        return swRegistration.sync.register("favorite").then(() => {
+          console.log("Favorite Sync registered");
+        });
       });
-    });
   });
 });
 
 /**
  * Add restaurant name to the breadcrumb navigation menu
  */
-fillBreadcrumb = (restaurant=self.restaurant) => {
-  const breadcrumb = document.getElementById('breadcrumb');
-  const li = document.createElement('li');
+fillBreadcrumb = (restaurant = self.restaurant) => {
+  const breadcrumb = document.getElementById("breadcrumb");
+  const li = document.createElement("li");
   li.innerHTML = restaurant.name;
   breadcrumb.appendChild(li);
-}
+};
 
 /**
  * Get a parameter by name from page URL.
  */
 getParameterByName = (name, url) => {
-  if (!url)
-    url = window.location.href;
-  name = name.replace(/[\[\]]/g, '\\$&');
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
   const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`),
     results = regex.exec(url);
-  if (!results)
-    return null;
-  if (!results[2])
-    return '';
-  return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
